58beee90b598e731e75799b5101e48f740b0279d
Masanori Maeda <gun534@yahoo.co.jp>
2023-11-13 01:31:55 +0900

    JTDXのソースを落としてくるURLをgithubからSourceForgeへ変更。

--- jtdx_install.sh
+++ jtdx_install.sh
@@ -39,7 +39,7 @@ echo $2 | sudo -S make install
 
 #mariadbのインストール
 echo $2 | sudo -S sudo apt -y install mariadb-server
-
+git 
 #hamlibのインストール
 cd ~
 mkdir ~/hamlib-prefix
@@ -54,13 +54,13 @@ make
 make install-strip
 
 #JTDXのインストール
-mkdir -p /home/$1/jtdx-prefix/build
-cd /home/$1/jtdx-prefix
-git clone https://github.com/jtdx-project/jtdx src
-cd /home/$1/jtdx-prefix/build
+mkdir -p ~/jtdx-prefix/build
+cd ~/jtdx-prefix
+git clone git://git.code.sf.net/p/jtdx/code src
+cd ~/jtdx-prefix/build
 echo $2 | sudo -S apt -y install build-essential cmake automake libtool gfortran pkg-config asciidoc asciidoctor qttools5-dev-tools qt5-default qtmultimedia5-dev libqt5serialport5-dev libudev-dev  libfftw3-dev git libhamlib-dev libhamlib-utils portaudio19-dev libqt5multimedia5-plugins coderay dialog libsamplerate0-dev python3-pip python3-tk python-dev-is-python2 python3-numpy python3-setuptools python3-dev subversion texinfo libfreetype6-dev libgtk-3-dev libgtk2.0-dev libjpeg-dev liblcms2-dev libtiff5-dev libwebp-dev tcl8.6-dev tk8.6-dev libusb-dev libusb-1.0-0-dev libgfortran4 libfftw3-3 libboost1.67-all-dev libqt5websockets5-dev
 
-cmake -D CMAKE_PREFIX_PATH=/home/$1/hamlib-prefix -D CMAKE_INSTALL_PREFIX=/home/$1/jtdx-prefix ../src
+cmake -D CMAKE_PREFIX_PATH=~/hamlib-prefix -D CMAKE_INSTALL_PREFIX=~/jtdx-prefix ../src
 cmake --build .
 echo $2 | sudo -S cmake --build . --target install
 
